14bbdec9c1916eaf0ad289a90b52c2f5e322e3e8
Israel Harvin <izzy84.ih@gmail.com>
2019-10-09 15:32:15 -0400

    fixing the bugs on lyric page, wondering why it is taking forever to load

--- lyric-finder/src/components/tracks/Lyrics.js
+++ lyric-finder/src/components/tracks/Lyrics.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import axios from "axios";
 import Spinner from "../layout/Spinner";
 import {Link} from "react-router-dom";
+import Moment from "react-moment";
 
 
 class Lyrics extends Component {
@@ -49,8 +50,15 @@ class Lyrics extends Component {
                             <strong>Album ID</strong>: {track.album_id}
                         </li>
                         <li className="list-group-item">
-                            <strong>Song Genre</strong>: 
-                            {/* {track.primary_genres.music_genre_list[0].music_genre.music_genre_name} */}
+                            <strong>Song Genre</strong>: {' '} 
+                            {track.primary_genres.music_genre_list[0].music_genre.music_genre_name}
+                        </li>
+                        <li className="list-group-item">
+                            <strong>Explicit Words</strong>: {track.explicit === 0 ? 'No' : 'Yes'}
+                        </li>
+                        <li className="list-group-item">
+                            <strong>Release Date</strong>: {' '}
+                            <Moment format="MM/DD/YYYY">{track.first_release_date}</Moment>
                         </li>
                     </ul>
                 </React.Fragment>
